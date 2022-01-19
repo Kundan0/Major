2a7ff42287350a74d15a43868355b801ecead42c
Kundan0 <074bct517.kundan@pcampus.edu.np>
2022-01-19 12:20:53 +0545

    thorai edited in train.py path variable

--- train.py
+++ train.py
@@ -6,7 +6,10 @@ from ClassModel import myModel
 from DeviceData import DeviceDataLoader
 import numpy as np
 
-PATH=os.curdir
+PATH=os.path.join("/content","drive","MyDrive")
+PATHJ=os.path.join("/content","Major")
+
+#PATH=os.curdir
 
 learn_type=2
 if learn_type==1:
@@ -21,17 +24,17 @@ else:
 print(model_name)
 device= torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')    
 print(device)    
-depth_dir=os.path.join(PATH,"Depth2")
+depth_dir=os.path.join(PATH,"Depth2","All")
 of_dir=os.path.join(PATH,"NewOpticalFlow")
 an_dir=os.path.join(PATH,"Annotations")
-json_dir=os.path.join(PATH,json_name)
+json_dir=os.path.join(PATHJ,json_name)
 
 dataset=myDataset(json_dir,depth_dir,of_dir,an_dir)
 dataset_size=len(dataset)
 train_size=int(dataset_size*0.9)
 train_ds, val_ds = random_split(dataset, [train_size,dataset_size-train_size])
 train_dl=DataLoader(train_ds,batch_size=4,shuffle=True)
-val_dl=DataLoader(val_ds,batch_size=1,shuffle=True)
+val_dl=DataLoader(val_ds,batch_size=4,shuffle=True)
 
 train_dl=DeviceDataLoader(train_dl,device)
 val_dl=DeviceDataLoader(val_dl,device)
@@ -46,7 +49,7 @@ def fit(epochs,model,train_dl,val_dl,learning_rate,optim=torch.optim.SGD):
     
     
     try:
-        model.load_state_dict(torch.load(os.path.join(PATH,"State",model_name)))
+        model.load_state_dict(torch.load(os.path.join(PATHJ,"State",model_name)))
         print("Loading Model ...")
     except:
         print("Cannot Load")
@@ -64,7 +67,7 @@ def fit(epochs,model,train_dl,val_dl,learning_rate,optim=torch.optim.SGD):
             optimizer.step() 
             if idx % 50 ==0 and idx!=0:
                 
-                torch.save(model.state_dict(),os.path.join(PATH,"State",model_name))
+                torch.save(model.state_dict(),os.path.join(PATHJ,"State",model_name))
                 print("saving model")
             history.append(l)
             print("average_Loss for last 50 batches",np.average(history[-50:]))
